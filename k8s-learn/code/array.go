190de5d505bb7154698a94210d3b9b3c2b7bc75d
codesunyong <1655603465@qq.com>
2024-01-08 16:40:15 +0800

    array

---
+++ k8s-learn/code/array.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := [5]string{"admin", "root", "admin1", "admin2", "root1"}
+	age := [5]int{10, 20, 30, 40, 50}
+	fmt.Println("name:", name)
+	fmt.Println("age:", age)
+}
