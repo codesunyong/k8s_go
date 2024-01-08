a0b12722e9aae459a28117dd08a4ca05509c2480
sunyong <1655603465@qq.com>
2024-01-08 17:36:51 +0800

    map

---
+++ k8s-learn/code/map.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+// map
+func main() {
+	//golang是强类型语言,所有数据都必须定义类型,除了interface以外,这个interface是定义所有的数据类型。
+	msg := map[string]interface{}{"username": "admin", "password": "12345"}
+	users := map[string]string{"username": "root", "username1": "admin"}
+	fmt.Println(msg, users)
+	//map 和struct有什么区别
+	Order := struct {
+		Uid  string
+		Type string
+		time string
+	}{}
+	type stu struct { ////type xxx struct 固定结构 这里的xxx是你的struct的名字
+		name string // k v k是什么？定义的数据结构的名字，v是什么？这个数据结构的数据类型
+		id   int
+	}
+	//struct的使用场景?固定好了的数据结构,不可以变更。
+	//map的使用场景?不用固定数据类型,可以随便增加减少。
+
+}
